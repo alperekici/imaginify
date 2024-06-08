70d6595f096fe8c01217043e389d88c954b68965
Varcolac1 <83782519+Varcolac1@users.noreply.github.com>
2024-06-08 20:09:18 +0300

    resolving bug

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,53 +1,41 @@
 import { Schema, model, models } from "mongoose";
 
-// interface IUser extends Document{
-//     clerkId: string;
-//     email: string;
-//     username: string;
-//     photo: ImageData;
-//     firstName: string;
-//     lastName: string;
-//     planId: number;
-//     creditBalance: number;
-// }
-
-
 const UserSchema = new Schema({
-    clerkId: { 
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: { 
-        type: String,
-        required: true, 
-        unique: true
-    },
-    username: { 
-        type: String, 
-        required: true,
-        unique: true
-    },
-    photo: { 
-        type: Image,
-        required: true
-    },
-    firstName: {
-        type: String, 
-    },
-    lastName: {
-        type: String,
-    },
-    planId: { 
-        type: Number, 
-        required: true, 
-        default: 1},
-    creditBalance: { 
-        type: Number, 
-        required: true, 
-        default: 10}
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
 });
 
-const User = models?.User || model('User', UserSchema);
+const User = models?.User || model("User", UserSchema);
 
 export default User;
